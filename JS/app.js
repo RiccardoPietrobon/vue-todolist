e2787e8ba2f7f07ae1e5b2360c23c58c9521dcb8
RiccardoPietrobon <riccardopietrobon.sanso@gmail.com>
2023-02-13 20:19:37 +0100

    milestone 1

--- JS/app.js
+++ JS/app.js
@@ -1,13 +1,67 @@
+// Rifare l'esercizio della to do list.
+// Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
+// - text, una stringa che indica il testo del todo
+// - done, un booleano(true / false) che indica se il todo è stato fatto oppure no
+//    ** MILESTONE 1 **
+// Stampare all'interno di una lista HTML un item per ogni todo.
+// Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+//      ** MILESTONE 2 **
+// Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
+//      ** MILESTONE 3 **
+// Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+//      ** Bonus:**
+// 1 - oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
+// 2 - cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
+// Buon lavoro e buon divertimento!(edited) 
+
+
+
 const app = Vue.createApp({
 
     data() {
         return {
+            tasks: [
+                {
+                    text: "Fai la spesa",
+                    done: true,
+                },
+                {
+                    text: "Dentista ore 15:00",
+                    done: true,
+                },
+                {
+                    text: "Meccanico mercoledì",
+                    done: false,
+                },
+                {
+                    text: "Comprare aspirapolvere",
+                    done: true,
+                },
+                {
+                    text: "Pulire la camera",
+                    done: true,
+                },
+                {
+                    text: "Prepara la cena",
+                    done: false,
+                },
+                {
+                    text: "Sistema garage",
+                    done: true,
+                },
+                {
+                    text: "Tagliare l'erba",
+                    done: false,
+                },
 
+            ]
 
         }
     },
 
-    //methods: {},
+    methods: {
+        if()
+    },
 
     //created() {},
 
